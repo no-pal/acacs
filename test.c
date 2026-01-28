57140b198a217553bd208d4a88b022696b8767c4
Lyndi <lyndiguerrero7@gmail.com>
2026-01-28 14:51:52 -0500

    yup

--- test.c
+++ test.c
@@ -1,9 +1,31 @@
-#include <stdio.h>
+#include <stdio.h>  
+void twenty()
+{
+for(int i=2; i<=20; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+void ten()
+{
+for(int i=0; i<=10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
+void forty()
+{
+    for(int i=2;i<=20;i++)
+    {
+        printf("%d\n", i);
+    }
+    
+       
+ }
 int main(){
-    void build_arr(){
-
-    }
-    void search_arr(){
-        
-    }
+{
+    twenty();
+    ten();
+    forty();
+}
 }
