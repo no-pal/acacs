421d335b8c8da640f4081dd6bd19a7e88a1a022e
Lyndi <lyndiguerrero7@gmail.com>
2026-02-20 14:36:04 -0500

    did

--- versionb.py
+++ versionb.py
@@ -1,6 +1,31 @@
 print(f"Python")
+points = 50
+print(points)
+points = 75
+print(point)
 m = 9
 n = 4
 print(m+n)
 print(m%n)
 side = 6
+print(36)
+First = "Grace"
+Last = "Hopper"
+print(f"{First} {Last}")
+a= 5
+b= 8
+print(a <= b)
+print(b == 10)
+has_fuel = True
+engine_check = False
+if has_fuel == True and not engine_check == False:
+    print("car can start")
+else 
+    print("car cannot start")
+val1 = True
+val2 = False
+print(val1 + val2)
+name = "Alice"
+blue_marbles = 12
+red_marbles = 8
+print(f"{name} has {blue_marbles + red_marbles}.")
