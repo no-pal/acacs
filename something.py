a95bfddd027c47b70a119dbff386799ab4b7675d
Lyndi <lyndiguerrero7@gmail.com>
2026-03-03 15:08:10 -0500

    did

---
+++ something.py
@@ -0,0 +1,6 @@
+def msg():
+    age = input("What's your age?")
+    name = input("What's your name?")
+    major = input("What's your major?")
+    print(f"Hello {name} you're {age} studying in {major}")
+def msg()
